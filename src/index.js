c67bbcdcc19da6c091c62c867e9d5e3c31946d3e
aadip26 <aadipawar4940@gmail.com>
2025-08-05 16:55:42 +0530

    add file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+
+
+
+app.listen(port, (req, res) =>{
+    console.log(`app listening at "http://localhost${port}"`)
+});
